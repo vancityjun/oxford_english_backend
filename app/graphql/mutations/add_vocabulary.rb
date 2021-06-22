417bf1391d164da5c81ce1da7e62b80fe81ea138
Jun <vancityjun@gmail.com>
2021-06-21 22:14:33 -0700

    add level argument to AddVocabulary mutation

--- app/graphql/mutations/add_vocabulary.rb
+++ app/graphql/mutations/add_vocabulary.rb
@@ -3,6 +3,7 @@ module Mutations
     argument :word, String, required: true
     argument :pos, String, required: true
     argument :celpip, Boolean, required: false
+    argument :level, String, required: false
 
     argument :definition_attributes, Types::DefinitionAttributes, required: false
     argument :examples, [Types::ExampleAttributes], required: false
@@ -10,12 +11,14 @@ module Mutations
     field :vocabulary, Types::VocabularyType, null: false
     field :errors, [String], null: false
 
-    def resolve(word:, pos:, celpip:, definition_attributes:, examples:)
+    def resolve(word:, pos:, celpip:, level:, definition_attributes:, examples:)
       current_user = context[:current_user]
       return unless current_user.admin
 
       vocabulary =  Vocabulary.find_or_initialize_by(word: word, pos: pos)
       vocabulary.celpip = celpip
+      vocabulary.level = level if vocabulary.new_record? && level.present?
+
       result = ActiveRecord::Base.transaction do
 
         current_user.vocabularies << vocabulary if current_user.vocabularies.find_by_id(vocabulary.id).nil?
